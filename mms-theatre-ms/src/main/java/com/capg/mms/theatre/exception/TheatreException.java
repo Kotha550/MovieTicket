75518864f93745e0c4987a148f695d6a6f54f94e
hp <hp@192.168.0.6>
2020-05-16 11:11:12 +0530

    commit

---
+++ mms-theatre-ms/src/main/java/com/capg/mms/theatre/exception/TheatreException.java
@@ -0,0 +1,16 @@
+package com.capg.mms.theatre.exception;
+
+
+
+public class TheatreException extends RuntimeException {
+
+	public TheatreException(String message)
+	{
+		super(message);
+	
+	}
+	public TheatreException()
+	{
+		super();
+	}
+}
